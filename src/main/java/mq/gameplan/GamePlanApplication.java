e209c51a1f49a965ceff157b59ffaa907a50b4c7
quaso <martinkvasnicka@gmail.com>
2018-09-16 10:27:49 +0200

    Initial commit

---
+++ src/main/java/mq/gameplan/GamePlanApplication.java
@@ -0,0 +1,12 @@
+package mq.gameplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePlanApplication.class, args);
+	}
+}
---
+++ src/test/java/mq/gameplan/GamePlanApplicationTests.java
@@ -0,0 +1,16 @@
+package mq.gameplan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamePlanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
